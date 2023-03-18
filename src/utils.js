d750383bde66543e64ef2bd62c2d059a112b28df
jopijuco <jpjc@protonmail.com>
2023-03-18 09:54:52 +0000

    improve utils

--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,8 @@ export const isOdd = (value) => value % 2 != 0;
 
 export const convertToDate = (text) => {
   const trimDate = text.trim().replace(/[.\s]/g, '');
-  const yyyyMmDdFormat = `${trimDate.slice(4)}-${trimDate.slice(2, 4)}-${trimDate.slice(0, 2)}`
-  return new Date(yyyyMmDdFormat);
+  return `${trimDate.slice(0, 2)}-${trimDate.slice(2, 4)}-${trimDate.slice(4)}`;
 }
+
+export const areObjectsEqual = (object1, object2) =>
+  JSON.stringify(object1) === JSON.stringify(object2);

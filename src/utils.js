97e6ca869ca90b3f405d8f7fd63c5986c55117a0
jopijuco <jpjc@protonmail.com>
2023-04-19 23:38:06 +0100

    Unformat date on conversion

--- src/utils.js
+++ src/utils.js
@@ -2,10 +2,7 @@ import * as dotenv from 'dotenv';
 
 export const isOdd = (value) => value % 2 != 0;
 
-export const convertToDate = (text) => {
-  const trimDate = text.trim().replace(/[.\s]/g, '');
-  return `${trimDate.slice(0, 2)}-${trimDate.slice(2, 4)}-${trimDate.slice(4)}`;
-}
+export const convertToDate = (text) => text.trim().replace(/[.\s]/g, '');
 
 export const areObjectsEqual = (object1, object2) =>
   JSON.stringify(object1) === JSON.stringify(object2);

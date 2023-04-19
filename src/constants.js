acf0c8c2c5eab76db93ba29c830f6678af00fb8b
jopijuco <jpjc@protonmail.com>
2023-04-19 23:37:37 +0100

    Fixed concurrency between several posts

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,3 @@
 export const PARLIAMENT_VOTING_ARCHIVE_URL = 'https://www.parlamento.pt/ArquivoDocumentacao/Paginas/Arquivodevotacoes.aspx';
 export const WEBSITE_VOTING_ELEMENT_ID = 'ctl00_ctl50_g_ef1010ac_1ad2_43f3_b329_d1623b7a1246_ctl00_pnlUpdate';
+export const VOTING_SCRIPT_TITLE = 'Guião de Votações';
--- src/discord-client.js
+++ src/discord-client.js
@@ -1,16 +1,60 @@
-const publishOnDiscord = async (post) => {
+const publishOnDiscord = async (unnotifiedPosts, db) => {
+  unnotifiedPosts.forEach(async (post, index) => {
+    const formattedPost = {
+      date: `${post.date.slice(0, 2)}-${post.date.slice(2, 4)}-${post.date.slice(4)}`,
+      title: post.title,
+      pdfUrl: post.pdfUrl
+    };
+    console.log(`Post: ${formattedPost} sent to be published.`);
+
+    setTimeout(async () => {
+      await postMessage(formattedPost);
+    }, 5000 * (index + 1));
+
+    if (index == 0) {
+      db.data.lastPost = formattedPost;
+      await db.write();
+      console.log('Last post was written to db.json.');
+    }
+    console.log('All new posts were published.');
+  })
+}
+
+const postMessage = async (message) => {
   const options = {
     method: 'POST',
-    body: JSON.stringify({
-      content: `[${post.title} ${post.date}](${post.pdfUrl})`
-    }),
+    body: buildRequestBody(message),
     headers: {
       'Content-Type': 'application/json'
     }
   };
 
   fetch(process.env.DISCORD_WEB_HOOK, options)
-    .then(response => console.log(response));
+    .then(async (response) => {
+      if (response.status === 429) {
+        console.log('Discord publish has failed. We will retry...');
+        await postMessage(message);
+      }
+      else {
+        console.log('Retry successful.');
+        return response.json();
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      return error;
+    });
+}
+
+const buildRequestBody = (message) => {
+  if (typeof content === 'string') {
+    return JSON.stringify({
+      content: message
+    });
+  }
+  return JSON.stringify({
+    content: `[${message.title} na data ${message.date}](${message.pdfUrl})`
+  });
 }
 
 export default publishOnDiscord;

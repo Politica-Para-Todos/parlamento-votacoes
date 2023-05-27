105ba28287220905bbb86a9d66a293c067a50215
jopijuco <jpjc@protonmail.com>
2023-05-27 16:48:50 +0100

    Stop message if no posts are found

--- src/execution.js
+++ src/execution.js
@@ -1,6 +1,5 @@
 import scan from './scan.js';
 import postOnDiscord from './discord-client.js';
-import { currentDateTime } from './utils.js';
 
 const executeApp = async () => {
   console.log('Requesting Parlamento/Arquivo.pt...');
@@ -9,7 +8,7 @@ const executeApp = async () => {
 
   if (unnotifiedPosts.length === 0) {
     console.log('There are no new posts.');
-    await postOnDiscord(`Olá :wave: \nHoje, ${ currentDateTime() }, confirmei e não existem novos resultados de votações. \nAmanhã procuro novamente, até lá !`);
+    process.exit();
   }
   await postOnDiscord(unnotifiedPosts);
 }
